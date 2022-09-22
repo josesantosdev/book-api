5d2cd3d93b1e4f589df1f973789c69372be67cfc
José Santos <thecaasantos@gmail.com>
2022-09-22 03:33:00 -0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class BaseConfig():
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = False
-
-class DevelopmentConfig(BaseConfig):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:wsr29yhi@localhost/books'
-    DATABASE_CONNECT_OPRTIONS = {}
-    JSON_AS_ASCII = False
