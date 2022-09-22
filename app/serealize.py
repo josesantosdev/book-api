de411ab0ff96ca81ee601c4106a49a2a7f6b26f8
casfalking <casfalking@pop-os.localdomain>
2022-09-22 03:34:27 -0300

    deleting comma

--- app/serealize.py
+++ app/serealize.py
@@ -40,6 +40,6 @@ class BookSchema(SQLAlchemyAutoSchema):
         autor_id = fields.Integer()
 
     _links = ma.Hyperlinks({
-        "colletion": ma.URLFor("books.show"),
+        "colletion": ma.URLFor("books.show")
     })
 
